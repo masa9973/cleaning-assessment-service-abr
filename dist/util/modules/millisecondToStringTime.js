aa4ea3021954d80ed3d512163f2d1b928cbb65f3
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-26 10:59:32 +0900

    ミリ秒計算修正

--- dist/util/modules/millisecondToStringTime.js
+++ dist/util/modules/millisecondToStringTime.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.millisecondToStringTime = void 0;
 function millisecondToStringTime(millisecond) {
-    const HH = Math.floor(millisecond / 86400000);
-    const mm = Math.floor(millisecond / 3600000);
-    const ss = Math.floor(millisecond / 60000);
+    const HH = Math.floor(millisecond / 3600000) % 24;
+    const mm = Math.floor(millisecond / 60000) % 60;
+    const ss = Math.floor(millisecond / 1000) % 60;
     return `${HH}時間${mm}分${ss}秒`;
 }
 exports.millisecondToStringTime = millisecondToStringTime;
