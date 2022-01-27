a3d0fd03d2c819d6b3e37ac60f234f81a728642e
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-27 10:33:23 +0900

    delete

--- dist/entities/repositories/modules/roomMastRepository.d.ts
+++ dist/entities/repositories/modules/roomMastRepository.d.ts
@@ -1,6 +1,7 @@
 import { Scalars, RoomMast } from '../../type';
 export interface IRoomMastRepository {
     addRoom(input: RoomMast): Promise<RoomMast>;
+    deleteRoom(roomID: Scalars['ID']): Promise<RoomMast>;
     fetchRoomsByHotelID(roomHotelID: Scalars['ID']): Promise<RoomMast[]>;
     fetchRoomByRoomID(roomID: Scalars['ID']): Promise<RoomMast | null>;
 }
--- dist/entities/repositories/modules/scoreItemMastRepository.d.ts
+++ dist/entities/repositories/modules/scoreItemMastRepository.d.ts
@@ -1,6 +1,7 @@
 import { Scalars, ScoreItemMast } from '../..';
 export interface IScoreItemMastRepository {
     addScoreItem(input: ScoreItemMast): Promise<ScoreItemMast>;
+    deleteScoreItem(scoreItemID: Scalars['ID']): Promise<ScoreItemMast>;
     fetchScoreItemsByHotelID(scoreItemHotelID: Scalars['ID']): Promise<ScoreItemMast[]>;
     fetchScoreItemByScoreItemID(scoreItemID: Scalars['ID']): Promise<ScoreItemMast | null>;
 }
