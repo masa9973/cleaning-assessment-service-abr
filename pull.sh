48f3666a6555ff8ecdf94cd22241d8f116ad6d18
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-03-20 13:37:13 +0900

    update name

--- pull.sh
+++ pull.sh
@@ -4,6 +4,6 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ $branch != 'exit' ]; then
     echo ""
     echo ""
-    echo "\033[43;31m$branch ABR stage3をFetchします。\033[0m"
-    yarn add git+ssh://git@github.com:masa9973/stage3-abr.git#${branch}
+    echo "\033[43;31m$branch ABR cleaning-assessment-serviceをFetchします。\033[0m"
+    yarn add git+ssh://git@github.com:masa9973/cleaning-assessment-service-abr.git#${branch}
 fi
