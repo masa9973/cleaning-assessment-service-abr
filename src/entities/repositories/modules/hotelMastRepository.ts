bd089423e970905d2039f19be824843153d10bfe
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-04-17 01:35:52 +0900

    delete HotelMast

--- src/entities/repositories/modules/hotelMastRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Scalars, HotelMast } from '../../type'
-
-export interface IHotelMastRepository {
-    addHotel(input: HotelMast): Promise<HotelMast>
-    fetchHotelByHotelID(hotelID: Scalars['ID']): Promise<HotelMast | null>
-}
