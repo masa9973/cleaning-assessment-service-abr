c7ac8c4e834bf18a01eb2608e33a6946ba684277
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-04-21 20:07:09 +0900

    add recordDateCache

--- src/adaptor/repositoryCacheAdaptor/recordMastRepository.ts
+++ src/adaptor/repositoryCacheAdaptor/recordMastRepository.ts
@@ -4,7 +4,7 @@ import { RecordMast } from '../../entities';
 
 type HotelIDRecordDateCache = {
     [hotelID: string]: {
-        [recordDate: string]: RecordMast[]
+        [recordDate: string]: RecordMast
     }
 }
 
@@ -27,20 +27,27 @@ type CleanerIDRoomIDCache = {
 }
 
 export class RecordMastRepositoryCacheAdaptor extends BaseCacheAdaptor implements IRecordMastRepository {
+    constructor(
+        private repository: IRecordMastRepository
+    ){
+        super();
+    }
     private hotelIDCaches: HotelIDCache[] = []
-    private hotelIDRecordDateCache: HotelIDRecordDateCache = {}
+    private hotelIDRecordDateCaches: HotelIDRecordDateCache[] = []
     private cleanerIDCache: CleanerIDCache = {}
     private recordIDCache: RecordIDCache = {}
     private cleanerIDRoomIDCache: CleanerIDRoomIDCache = {}
     cacheClear(): void {
         this.hotelIDCaches = []
-        this.hotelIDRecordDateCache  = {}
+        this.hotelIDRecordDateCaches  = []
         this.cleanerIDCache = {}
         this.recordIDCache = {}
         this.cleanerIDRoomIDCache = {}
     }
     async addRecord(input: RecordMast): Promise<RecordMast> {
+        // 全キャッシュを保存
         this.addHotelIDCache(input)
+        this.addHotelIDRecordDateCache(input)
         const res = await this.addRecord(input)
         return res
     }
@@ -54,12 +61,17 @@ export class RecordMastRepositoryCacheAdaptor extends BaseCacheAdaptor implement
             return this.hotelIDCaches.map((item) => item[recordHotelID])
         }
         // ないならキャッシュを追加
-        const res = await this.fetchAllRecordsByHotelID(recordHotelID)
+        const res = await this.repository.fetchAllRecordsByHotelID(recordHotelID)
         this.addHotelIDCaches(res)
         return res
     }
     async fetchRecordsByDate(recordHotelID: string, recordDate: string): Promise<RecordMast[]> {
-        throw new Error('Method not implemented.');
+        if (this.hotelIDRecordDateCaches) {
+            return this.hotelIDRecordDateCaches.map((item) => item[recordHotelID][recordDate])
+        }
+        const res = await this.repository.fetchRecordsByDate(recordHotelID, recordDate)
+        this.addHotelIDRecordDateCaches(res)
+        return res
     }
     async fetchRecordByRecordID(recordID: string): Promise<RecordMast | null> {
         throw new Error('Method not implemented.');
@@ -77,5 +89,20 @@ export class RecordMastRepositoryCacheAdaptor extends BaseCacheAdaptor implement
 
     private addHotelIDCache(input: RecordMast) {
         this.hotelIDCaches.push({[input.recordHotelID]: input})
+    }
+
+    private addHotelIDRecordDateCache(input: RecordMast) {
+        const arg: HotelIDRecordDateCache = {
+            [input.recordHotelID]: {
+                [input.recordDate]: input
+            }
+        }
+        this.hotelIDRecordDateCaches.push(arg)
+    }
+
+    private addHotelIDRecordDateCaches(input: RecordMast[]) {
+        input.map((item) => {
+            this.addHotelIDRecordDateCache(item)
+        })
     }
 }
